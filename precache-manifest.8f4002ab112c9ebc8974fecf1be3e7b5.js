1d1536965802ffdd9f16b5299bdde90d6dbf7291
Serg <sergeipaderov@gmail.com>
2020-04-23 20:59:38 -0300

    Updates

---
+++ precache-manifest.8f4002ab112c9ebc8974fecf1be3e7b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51837f0aa3f86c3175f599b550115556",
+    "url": "/sergeipaderov/C-19-Tracker/index.html"
+  },
+  {
+    "revision": "8482019fd1cec1e03a68",
+    "url": "/sergeipaderov/C-19-Tracker/static/css/main.abcc616f.chunk.css"
+  },
+  {
+    "revision": "aa8d5b50e14ed883dbee",
+    "url": "/sergeipaderov/C-19-Tracker/static/js/2.098bf0a0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/sergeipaderov/C-19-Tracker/static/js/2.098bf0a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8482019fd1cec1e03a68",
+    "url": "/sergeipaderov/C-19-Tracker/static/js/main.164f3893.chunk.js"
+  },
+  {
+    "revision": "a1aacc9655b037c2bbec",
+    "url": "/sergeipaderov/C-19-Tracker/static/js/runtime-main.abdc8f4e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/sergeipaderov/C-19-Tracker/static/media/image.d7265326.png"
+  }
+]);
